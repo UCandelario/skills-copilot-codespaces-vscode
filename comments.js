71b96002ea572df482f69a0e201c351053dc1270
Ulises-Candelario <urcs2019@gmail.com>
2024-02-14 12:18:23 -0600

    Copilot third commit

--- comments.js
+++ comments.js
@@ -11,4 +11,93 @@ http.createServer(function (req, res) {
         res.write(data);
         res.end();
     });
-}).listen(8080);
+}).listen(8080); //listen on port 8080
+console.log('Server is running on http://localhost:8080/');
+//end of the server
+
+//read the file
+var fs = require('fs');
+var comments = require('./comments.json');
+
+//create server
+http.createServer(function (req, res) {
+    if (req.method === 'POST') {
+        var body = '';
+
+        req.on('data', function (chunk) {
+            body += chunk;
+        });
+
+        req.on('end', function () {
+            fs.readFile('comments.json', function (err, data) {
+                var comments = JSON.parse(data);
+                comments.push(JSON.parse(body));
+                fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function (err) {
+                    console.log('Comment added');
+                });
+            });
+        });
+    }
+}).listen(8080); //listen on port 8080
+console.log('Server is running on http://localhost:8080/');
+//end of the server
+
+//read the file
+var fs = require('fs');
+var comments = require('./comments.json');
+
+//create server
+http.createServer(function (req, res) {
+    if (req.method === 'POST') {
+        var body = '';
+
+        req.on('data', function (chunk) {
+            body += chunk;
+        });
+
+        req.on('end', function () {
+            fs.readFile('comments.json', function (err, data) {
+                var comments = JSON.parse(data);
+                comments.push(JSON.parse(body));
+                fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function (err) {
+                    console.log('Comment added');
+                });
+            });
+        });
+    }
+
+    if (req.method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify(comments));
+        res.end();
+    }
+}).listen(8080); //listen on port 8080
+console.log('Server is running on http://localhost:8080/');
+//end of the server
+
+//read the file
+var fs = require('fs');
+var comments = require('./comments.json');
+
+//create server
+http.createServer(function (req, res) {
+    if (req.method === 'POST') {
+        var body = '';
+
+        req.on('data', function (chunk) {
+            body += chunk;
+        });
+
+        req.on('end', function () {
+            fs.readFile('comments.json', function (err, data) { // Added closing single quote here
+                var comments = JSON.parse(data);
+                comments.push(JSON.parse(body));
+                fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function (err) {
+                    console.log('Comment added');
+                });
+            });
+        });
+    }
+}).listen(8080); //listen on port 8080
+console.log('Server is running on http://localhost:8080/');
+//end of the server
